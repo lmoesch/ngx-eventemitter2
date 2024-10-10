95c31db0037975a13196e3e0c67c50c17cda9e83
Lucas Mösch <lmoesch@docsinclouds.com>
2024-10-10 14:41:19 +0200

    initial commit

---
+++ lib/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of ngx-eventemitter2
+ */
+
+export * from './src/ngx-eventemitter2.service';
+export * from './src/ngx-eventemitter2.component';
---
+++ lib/src/ngx-eventemitter2.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-eventemitter2',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      ngx-eventemitter2 works!
+    </p>
+  `,
+  styles: ``
+})
+export class NgxEventemitter2Component {
+
+}
